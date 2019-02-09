4aafa9523d74bb37a2adeddd172d205fe690c44e
akashchakraborty <33716183+akashchakraborty@users.noreply.github.com>
2019-02-09 21:16:00 +0530

    Add files via upload

---
+++ Project.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE appy;
+
+USE appy;
+
+CREATE TABLE bill(
+	name VARCHAR(30),
+    contact VARCHAR(20),
+    ChilliP INT,
+    FrenchF INT,
+    MomoV INT,
+    MomoC INT,
+    BurgerV INT,
+    BurgerC INT,
+    SandwichV INT,
+    SandwichC INT,
+    Omlette INT,
+    Maggie INT,
+    HotDog INT,
+    SubTot DOUBLE,
+    Deli DOUBLE,
+    TaxtTot DOUBLE,
+    Tot DOUBLE
+);
+
+
+SELECT * FROM bill;
